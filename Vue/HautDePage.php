fbced5bc05ce9e8b333e55186389ecdd3a6bb9e0
Salomé <salome.catteau@2023.icam.fr>
2022-10-10 11:41:10 +0200

    reorganisation

--- Vue/HautDePage.php
+++ Vue/HautDePage.php
@@ -15,24 +15,29 @@
             </form>-->
 			<a href="../index.php" class="buton" target="_self">MAIAT</a>
 		</div>
+
 		<div class = "bouton">
-			<form action = "../Controleur/critere.php">
-				<button type="submit" value="Les critères de risques sociaux" class="button"> 
+			<form action = "../Confiance/Controleur/critere.php">
+				<button type="submit" value="Les critères de risques sociaux" class="button">
+                    Les critères de risques sociaux
 			</form>
 		</div>
 		<div class = "bouton">
-			<form action = "../Controleur/guide.php">
-				<button type="submit" value="Le guide de MAIAT" class="button"> 
+			<form action = "../Confiance/Controleur/guide.php">
+				<button type="submit" value="Le guide de MAIAT" class="button">
+                    Le guide de MAIAT
 			</form>	
 		</div>
 		<div class = "bouton">
-			<form action = "../Controleur/projet.php">
-				<button type="submit" value="Equipes et Partenaires" class="button"> 
+			<form action = "../Confiance/Controleur/projet.php">
+				<button type="submit" value="Equipes et Partenaires" class="button">
+                    Equipes et Partenaires
 			</form>	
 		</div>
 		<div class = "bouton">
-			<form action = "../Controleur/identification.php">
-				<button type="submit" value="C'est à vous" class="button"> 
+			<form action = "../Confiance/Controleur/identification.php">
+				<button type="submit" value="C'est à vous" class="button">
+                    C'est à vous
 			</form>	
 		</div>
 	
