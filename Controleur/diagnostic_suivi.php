8c24880f068abf528a3d000681030c1ecf16e585
charles <charles.cayzac@2022.icam.fr>
2022-10-12 15:11:47 +0200

    modif visualiser les diagrammes

--- Controleur/diagnostic_suivi.php
+++ Controleur/diagnostic_suivi.php
@@ -18,34 +18,26 @@
 
 </head>
 <body>
-    <h4> Tous vos diagnostics : </h4>
+    <form action="comparaison_diagnostic.php" method="post" target="_self">
     <?php
-    $Id_Utilisateur = $_SESSION['id_Utilisateur'];
-
-    $requete = "SELECT Id_critere FROM Repondre where Id_utilisateur = '".$Id_Utilisateur."' ";
-    $resultat = mysqli_query($db,$requete);
-    $row = mysqli_fetch_assoc($resultat) ;
-
-    echo $row['Id_critere'];
-    $resultat2 = mysqli_query($db, "SELECT  a.Nom, b.Prenom FROM Criteres a, Utilisateurs b NATURAL JOIN Repondre c WHERE c.Id_critere = a.Id_critere AND c.Id_utilisateur = '$Id_Utilisateur' " ) or die ( "<br>BUG".mysqli_error($link));
-    $row2 = mysqli_fetch_assoc($resultat2) ;
-    while ( $uneLigne = mysqli_fetch_assoc($resultat2) )
-    {
-
-        ?>
-        <tr>
-            <td><b><?php print($uneLigne['Nom']);?></b></td>
-            <td><b><?php print($uneLigne['Prenom']);?></b></td>
-        </tr>
-        <?php
-    }
+        $id_utilisateur = $_SESSION['id_Utilisateur'];
+        $resultat2 = mysqli_query($db, "SELECT  a.Nom, b.Prenom FROM Criteres a, Utilisateurs b NATURAL JOIN Repondre c WHERE c.Id_critere = a.Id_critere AND c.Id_utilisateur = '$id_utilisateur'") or die ( "<br>BUG".mysqli_error($link));
+        $row2 = mysqli_fetch_assoc($resultat2) ;
     ?>
-
-
-
-
+        <h4> <?= $row2['Prenom']; ?> voici vos diagnostics : </h4>
     <?php
-    //$rq = "INSERT INTO `Repondre` VALUES ('$Id_Utilisateur', '$Id_Critere')" ;
+        while ($uneLigne=mysqli_fetch_assoc($resultat2)){
+            ?>
+                <tr>
+                    <td><br><?=($uneLigne['Nom']);?><input type="checkbox" value=".$uneLigne." /></br></td>
+                </tr>
+                <?php
+            }
+        ?>
+        <button type="submit">Afficher</button>
+    </form>
+
+    <!--$rq = "INSERT INTO `Repondre` VALUES ('$Id_Utilisateur', '$Id_Critere')" ;
     //$result =mysqli_query( $db, $rq )or die (mysqli_error($link));
 
 
@@ -55,5 +47,4 @@
 
     //$_SESSION['id_Critere']=$row['Id_critere'];
     //echo $_SESSION['id_Critere'];
-    //echo $_SESSION['id_Utilisateur'];
-    ?>
+    //echo $_SESSION['id_Utilisateur']; -->
