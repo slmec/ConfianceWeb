5a24b125c1d4eee250e983f1f68a1ba39959e686
Salomé <salome.catteau@2023.icam.fr>
2022-10-17 09:06:13 +0200

    modif text index

--- index.php
+++ index.php
@@ -17,8 +17,6 @@
 
         <video src="Medias/video_site.mp4" width=640 height=480 controls poster="Medias/vignette_video_site.jpg" class="video"></video>
 
-        <p>MAIAT est l&#39;outil pour int&eacute;grer l&#39;IA au travail en pleine confiance.</p>
-
         <p>De nombreux travaux appr&eacute;hendent le d&eacute;ploiement de l&rsquo;IA dans les organisations et les m&eacute;tiers.&nbsp;Au-del&agrave; des dynamiques destructions/cr&eacute;ations d&rsquo;emplois, elles d&eacute;gagent des questions de transformation du travail et de leur acceptabilit&eacute;. Elles pointent en ce sens sur la n&eacute;cessit&eacute; d&rsquo;&eacute;tablir une confiance technique et sociale envers l&rsquo;IA. Cet outil propose un diagnostic social des applications de l&rsquo;IA au travail en vue de faciliter l&rsquo;instauration d&rsquo;une confiance situ&eacute;e.</p>
 
     </section>
