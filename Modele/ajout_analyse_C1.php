0360d8de4dfdbfb8279d15596184d2930f953ef6
Salomé <salome.catteau@2023.icam.fr>
2022-10-17 16:47:58 +0200

    note c1q1

--- Modele/ajout_analyse_C1.php
+++ Modele/ajout_analyse_C1.php
@@ -12,10 +12,16 @@ $link =  mysqli_connect("localhost", "eleve.tou", "et*301");
 mysqli_select_db($link, "Confiance" );
 if ( ! $link ) die( "Impossible de se connecter à MySQL" );
 
-    $C1_interpretation = $_POST['C1_interpretation'];
-    $C1_plan_action = $_POST['C1_plan_action'];
-    $C1_suivi = $_POST['C1_suivi'];
-    $Id_Critere = $_SESSION['id_Critere'];
+$C1_interpretation = $_POST['C1_interpretation'];
+$C1_plan_action = $_POST['C1_plan_action'];
+$C1_suivi = $_POST['C1_suivi'];
+$Id_Critere = $_SESSION['id_Critere'];
 
-$requete = "UPDATE Diagnostics SET C1_interpretation='$C1_interpretation' WHERE Id_critere_bis = '$Id_Critere' ";
-$result = mysqli_query($db, $requete);
+if($C1_interpretation !== "" ) {
+
+
+    $requete = "UPDATE Diagnostics SET C1_interpretation ='$C1_interpretation' WHERE Id_critere_bis = '$Id_Critere' ";
+    $result = mysqli_query($link, $requete);
+    header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/Resultats_Diagnostic.php?'); // utilisateur ou mot de passe incorrect
+}
+    ?>
