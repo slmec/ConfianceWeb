45e9c039d99b7169c13cb190f08b2ccbdf44b067
charles <charles.cayzac@2022.icam.fr>
2022-10-17 14:30:36 +0200

    modif sans identification

---
+++ Controleur/tesquestionnaire_sansid.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title></title>
+</head>
+<body>
+
+<form action="../Modele/verification_questionnaire_sansid.php" method="post" name="Fragilisation_Reconnaissance" target="_self">
+    <p>Entrez le nom de votre diagnostic :&nbsp;
+        <input maxlength="250" name="Nom_Diagnostic" type="text" />
+    </p>
+    <h1>La reconnaissance : </h1>
+    <p>Le système à base d'IA réduit-il la distinction entre les novices et les experts ?
+        Oui <input type="radio" name="C1Q1" value="0" checked>
+        Non <input type="radio" name="C1Q1" value="1">  <br>
+    </p>
+    <p>Des tâches requérant auparavant de l'expertise sont-elles désormais partiellement ou totalement automatisées?
+        Oui <input type="radio" name="C1Q2" value="0" checked>
+        Non <input type="radio" name="C1Q2" value="1">  <br>
+    </p>
+    <p>Le système à base d'IA supprime-t-il des tâches pénibles, répétitives ou dangereuses ?
+        Oui <input type="radio" name="C1Q3" value="0" checked>
+        Non <input type="radio" name="C1Q3" value="1">  <br>
+    </p>
+    <p>L'introduction de la technologie rend-elle moins visible le résultat de l'activité du travailleur ?
+        Oui <input type="radio" name="C1Q4" value="0" checked>
+        Non <input type="radio" name="C1Q4" value="1">  <br>
+    </p>
+
+    <h1>Les relations humaines : </h1>
+    <p>La technologie introduit-elle une communication entre des machines ?
+        Oui <input type="radio" name="C2Q1" value="0" checked>
+        Non <input type="radio" name="C2Q1" value="1">  <br>
+    </p>
+    <p>La technologie crée-t-elle une interaction humain-machine au détriment d'une communication entre personnes ?
+        Oui <input type="radio" name="C2Q2" value="0" checked>
+        Non <input type="radio" name="C2Q2" value="1">  <br>
+    </p>
+    <p>La technologie intervient-elle dans la communication entre plusieurs personnes ?
+        Oui <input type="radio" name="C2Q3" value="0" checked>
+        Non <input type="radio" name="C2Q3" value="1">  <br>
+    </p>
+    <p>Le système à base d'IA impose-t-il des lexiques et des syntaxes standardisés pour communiquer ?
+        Oui <input type="radio" name="C2Q4" value="0" checked>
+        Non <input type="radio" name="C2Q4" value="1">  <br>
+    </p>
+
+    <h1>La surveillance : </h1>
+    <p>Le système à base d'IA intègre-t-il une caméra/micro susceptible de filmer/écouter le travailleur ou d’être perçu comme tel ?
+        Oui <input type="radio" name="C3Q1" value="0" checked>
+        Non <input type="radio" name="C3Q1" value="1">  <br>
+    </p>
+    <p>La technologie implique-t-elle des identifiants permettant de collecter des données sur son utilisateur ?
+        Oui <input type="radio" name="C3Q2" value="0" checked>
+        Non <input type="radio" name="C3Q2" value="1">  <br>
+    </p>
+    <p>Les données collectées par le système à base d'IA sont-elles exploitées pour mesurer la productivité de son utilisateur ?
+        Oui <input type="radio" name="C3Q3" value="0" checked>
+        Non <input type="radio" name="C3Q3" value="1">  <br>
+    </p>
+    <p>La finalité de l’utilisation des données est-elle transparente ?
+        Oui <input type="radio" name="C3Q4" value="1" checked>
+        Non <input type="radio" name="C3Q4" value="0">  <br>
+    </p>
+
+    <h1>L'autonomie : </h1>
+    <p>Le système à base d'IA détermine-t-il un déroulement de l’action du travailleur ?
+        Oui <input type="radio" name="C4Q1" value="0" checked>
+        Non <input type="radio" name="C4Q1" value="1">  <br>
+    </p>
+    <p>Le système à base d'IA émet-il des notifications à l’adresse du travailleur ?
+        Oui <input type="radio" name="C4Q2" value="0" checked>
+        Non <input type="radio" name="C4Q2" value="1">  <br>
+    </p>
+    <p>Le système à base d'IA réduit-il ou rend-il plus difficile la prise d’initiative pour le travailleur ?
+        Oui <input type="radio" name="C4Q3" value="0" checked>
+        Non <input type="radio" name="C4Q3" value="1">  <br>
+    </p>
+    <p>Le travailleur dispose-t-il de marge manœuvre convenue dans l’utilisation ou l’interprétation du système à base d'IA ?
+        Oui <input type="radio" name="C4Q4" value="1" checked>
+        Non <input type="radio" name="C4Q4" value="0">  <br>
+    </p>
+
+    <h1>Le savoir-faire : </h1>
+    <p>Le système à base d'IA modifie t-il l'équilibre entre intervention directe sur le produit et supervision de la machine?
+        Oui <input type="radio" name="C5Q1" value="0" checked>
+        Non <input type="radio" name="C5Q1" value="1">  <br>
+    </p>
+    <p>La technologie rend-elle l'activité plus facile à réaliser par tout un chacun?
+        Oui <input type="radio" name="C5Q2" value="0" checked>
+        Non <input type="radio" name="C5Q2" value="1">  <br>
+    </p>
+    <p>Le système à base d'IA rend-il des savoir-faire obsolètes ?
+        Oui <input type="radio" name="C5Q3" value="0" checked>
+        Non <input type="radio" name="C5Q3" value="1">  <br>
+    </p>
+    <p>Le système à base d'IA génère t-il de nouvelles tâches pour le travailleur?
+        Oui <input type="radio" name="C5Q4" value="0" checked>
+        Non <input type="radio" name="C5Q4" value="1">  <br>
+    </p>
+
+    <h1>La responsabilité : </h1>
+    <p>L’imputation de responsabilités en cas de problème est-elle un enjeu majeur de l’activité et de l’organisation ?
+        Oui <input type="radio" name="C6Q1" value="0" checked>
+        Non <input type="radio" name="C6Q1" value="1">  <br>
+    </p>
+    <p>Le système à base d'IA utilise des algorithmes d’apprentissage lui permettant de s’adapter de façon autonome dans un environnement aléatoire ?   Oui
+        Oui <input type="radio" name="C6Q2" value="0" checked>
+        Non <input type="radio" name="C6Q2" value="1">  <br>
+    </p>
+    <p>Le système à base d'IA réduit-il la liberté de prise d'initiative du travailleur?   Oui
+        Oui <input type="radio" name="C6Q3" value="0" checked>
+        Non <input type="radio" name="C6Q3" value="1">  <br>
+    </p>
+    <p>Pensez-vous que le système à base d'IA pourrait pousser le travailleur à moins s'investir dans ses tâches et/ou de ses responsabilités? Oui
+        Oui <input type="radio" name="C6Q4" value="0" checked>
+        Non <input type="radio" name="C6Q4" value="1">  <br>
+    </p>
+
+    <input type="submit" value="Valider les réponses">
+
+</form>
+
+<?php
+if(isset($_GET['erreur'])){
+    $err = $_GET['erreur'];
+    if($err==1 || $err==2)
+        echo "<p style='color:red'>Veuillez completer tous les champs </p>";
+}
+?>
+
+
+</body>
+</html>
+
---
+++ Modele/verification_questionnaire_sansid.php
@@ -0,0 +1,145 @@
+<?php
+session_start();
+
+// connexion à la base de données
+$db_username = 'eleve.tou';
+$db_password = 'et*301';
+$db_name     = 'Confiance';
+$db_host     = 'localhost';
+$db = mysqli_connect($db_host, $db_username, $db_password,$db_name);
+
+// note C1
+$i = 1;
+$a = 0;
+while ($i <= 4) {
+    if (isset($_POST['C1Q' . $i])) {
+        $Oui[$i] = intval($_POST['C1Q' . $i]);
+        $a = $a + $Oui[$i];
+        $i++;
+    } else {
+        unset($_POST['C1Q' . $i]);
+        $i++;
+    }
+}
+$_SESSION['critere_fragilisation_reconnaissance'] = $a;
+
+//pour afficher les implications
+$C1Q1 = $_POST['C1Q1'];
+$C1Q2 = $_POST['C1Q2'];
+$C1Q3 = $_POST['C1Q3'];
+$C1Q4 = $_POST['C1Q4'];
+
+
+/* $o = 1;
+$b = 0;
+while ($o <= 4) {
+   if (isset($_POST['Non_C1Q' . $o])) {
+       $Non[$o] = intval($_POST['Non_C1Q' . $o]);
+       $b = $b + $Non[$o];
+       $o++;
+   } else {
+       unset($_POST['Non_C1Q' . $o]);
+       $o++;
+   }
+
+   $_SESSION['critere_fragilisation_reconnaissance'] = $a + $b;
+
+} */
+// note C2
+$i = 1;
+$a = 0;
+while ($i <= 4) {
+    if (isset($_POST['C2Q' . $i])) {
+        $Oui[$i] = intval($_POST['C2Q' . $i]);
+        $a = $a + $Oui[$i];
+        $i++;
+    } else {
+        unset($_POST['C2Q' . $i]);
+        $i++;
+    }
+}
+
+$_SESSION['critere_Desengagement_Relationnel'] = $a;
+
+$C2Q1 = $_POST['C2Q1'];
+$C2Q2 = $_POST['C2Q2'];
+$C2Q3 = $_POST['C2Q3'];
+$C2Q4 = $_POST['C2Q4'];
+
+// note C3
+$i = 1;
+$a = 0;
+while ($i <= 4) {
+    if (isset($_POST['C3Q' . $i])) {
+        $Oui[$i] = intval($_POST['C3Q' . $i]);
+        $a = $a + $Oui[$i];
+        $i++;
+    } else {
+        unset($_POST['C3Q' . $i]);
+        $i++;
+    }
+}
+
+$_SESSION['critere_Surveillance'] = $a;
+
+$C3Q1 = $_POST['C3Q1'];
+$C3Q2 = $_POST['C3Q2'];
+$C3Q3 = $_POST['C3Q3'];
+$C3Q4 = $_POST['C3Q4'];
+
+// note C4
+$i = 1;
+$a = 0;
+while ($i <= 4) {
+    if (isset($_POST['C4Q' . $i])) {
+        $Oui[$i] = intval($_POST['C4Q' . $i]);
+        $a = $a + $Oui[$i];
+        $i++;
+    } else {
+        unset($_POST['C4Q' . $i]);
+        $i++;
+    }
+}
+
+$_SESSION['critere_Perte_Autonomie'] = $a;
+
+// note C5
+$i = 1;
+$a = 0;
+while ($i <= 4) {
+    if (isset($_POST['C5Q' . $i])) {
+        $Oui[$i] = intval($_POST['C5Q' . $i]);
+        $a = $a + $Oui[$i];
+        $i++;
+    } else {
+        unset($_POST['C5Q' . $i]);
+        $i++;
+    }
+}
+$_SESSION['critere_Sentiment_Depossession'] = $a;
+
+// note C6
+$i = 1;
+$a = 0;
+while ($i <= 4) {
+    if (isset($_POST['C6Q' . $i])) {
+        $Oui[$i] = intval($_POST['C6Q' . $i]);
+        $a = $a + $Oui[$i];
+        $i++;
+    } else {
+        unset($_POST['C6Q' . $i]);
+        $i++;
+    }
+}
+$_SESSION['critere_Deresponsabilite'] = $a;
+
+$critere1 = $_SESSION['critere_fragilisation_reconnaissance'];
+$critere2 = $_SESSION['critere_Desengagement_Relationnel'];
+$critere3 = $_SESSION['critere_Surveillance'];
+$critere4 = $_SESSION['critere_Perte_Autonomie'];
+$critere5 = $_SESSION['critere_Sentiment_Depossession'];
+$critere6 = $_SESSION['critere_Deresponsabilite'];
+$_SESSION['Nom_Diagnostic'] = $_POST['Nom_Diagnostic'];
+
+header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/Resultats_Diagnostic.php');
+?>
