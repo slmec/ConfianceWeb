0aa3adec5c8f7999e8951f25acd7033787775ff0
charles <charles.cayzac@2022.icam.fr>
2022-10-18 09:31:07 +0200

    modif verification nom diagnostic

--- Modele/verification_nom_diagnostique.php
+++ Modele/verification_nom_diagnostique.php
@@ -8,8 +8,7 @@ $db_password = 'et*301';
 $db_name     = 'Confiance';
 $db_host     = 'localhost';
 
-$db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
-or die('could not connect to database');
+$db = mysqli_connect($db_host, $db_username, $db_password,$db_name) or die('could not connect to database');
 
 // diagnostic_new.php
     if(isset($_POST['Nom_Diagnostic'])) {
@@ -21,44 +20,23 @@ or die('could not connect to database');
         $resultat = mysqli_query($db, "SELECT Id_utilisateur FROM Utilisateurs WHERE Email = '".$email_Utilisateur."' AND MotDePasse = '".$mdp_Utilisateur."'");
         $row = (mysqli_fetch_assoc($resultat));
         $Id_Utilisateur = $row['Id_utilisateur'];
-        echo $Id_Utilisateur;
-        $resultat2 = mysqli_query($db, "SELECT a.Nom FROM Criteres a NATURAL JOIN Repondre b WHERE b.Id_utilisateur = '".$Id_Utilisateur."' AND b.Id_critere = a.Id_critere");
+        $resultat2 = mysqli_query($db, "SELECT a.Nom FROM Criteres a NATURAL JOIN Repondre b WHERE b.Id_utilisateur = '".$Id_Utilisateur."' AND b.Id_critere = a.Id_critere AND a.Nom = '".$Nom_Diagnostic."'");
 
-        if ($Nom_Diagnostic !== "") {
-            //On insère le nom du diagnostic dans la table diagnostic
-            //$requete = "INSERT INTO Criteres VALUES ('','','','','','','','','','','','','".$Nom_Diagnostic."')";
-            //$resultat = mysqli_query($db, $requete);
-
-            //On creer la variable session de l'id
-            //$requete2 = "SELECT Id_critere FROM Criteres WHERE Nom = '$Nom_Diagnostic'";
-            //$resultat2 = mysqli_query($db, $requete2);
-            //$row = mysqli_fetch_assoc($resultat2);
-            //$_SESSION['id_Critere'] = $row['Id_critere'];
-            while ($test = mysqli_fetch_assoc($resultat2)){
-                if ($test['Nom'] == $Nom_Diagnostic){
-                    header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/diagnostic_new.php?erreur=2');
-                }
-                else{
-                    header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/Questions_prequestionnaire.php');
-                }
+        if ($Nom_Diagnostic !== ""){
+            $test = mysqli_fetch_assoc($resultat2);
+            if ($test['Nom'] == $Nom_Diagnostic){
+                header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/diagnostic_new.php?erreur=2');
             }
-
+            else{
+                header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/Questions_prequestionnaire.php');
+            }
         }
         else {
-            header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/diagnostic_new.php?erreur=1'); // nom du diagnostique vide
+            header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/diagnostic_new.php?erreur=1'); // nom du diagnostic vide
         }
     }
     else{
         header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/diagnostic_new.php?');
     }
-
-
-
-
-
-
-// Fragilisation_Reconnaissance.php
-
-
 mysqli_close($db); // fermer la connexion
 ?>
