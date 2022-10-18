9c98e6c71731af383687717d0470f48186c4fb76
charles <charles.cayzac@2022.icam.fr>
2022-10-18 16:00:53 +0200

    modif inscription

--- Controleur/inscription2.php
+++ Controleur/inscription2.php
@@ -17,43 +17,42 @@
 
 </head>
 <body>
-<?php
-    $nom_utilisateur = $_POST['nom_utilisateur'];
-    $prenom_utilisateur = $_POST['prenom_utilisateur'];
-    $email_utilisateur = $_POST['email_utilisateur'];
-    $mdp_utilisateur = $_POST['mdp_utilisateur'];
-    /*$mdp_utilisateur_hash = password_hash($mdp_utilisateur,PASSWORD_DEFAULT);*/
-    $role_utilisateur = $_POST['role_utilisateur'];
-    $organisme_utilisateur = $_POST['organisme_utilisateur'];
+    <?php
+        $nom_utilisateur = $_POST['nom_utilisateur'];
+        $prenom_utilisateur = $_POST['prenom_utilisateur'];
+        $email_utilisateur = $_POST['email_utilisateur'];
+        $mdp_utilisateur = $_POST['mdp_utilisateur'];
+        /*$mdp_utilisateur_hash = password_hash($mdp_utilisateur,PASSWORD_DEFAULT);*/
+        $role_utilisateur = $_POST['role_utilisateur'];
+        $organisme_utilisateur = $_POST['organisme_utilisateur'];
 
-    if($nom_utilisateur !== "" && $prenom_utilisateur !== "" && $email_utilisateur !== "" && $mdp_utilisateur !== "" && $role_utilisateur !== "" && $organisme_utilisateur !== ""){
-        $result = mysqli_query($db,"SELECT Email FROM Utilisateurs WHERE Email = '".$email_utilisateur."'");
-        $respond = mysqli_fetch_assoc($result);
-        if ($respond['Email'] == $email_utilisateur){
-            header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/inscription.php?erreur=2');
-        }
-        else{
-            if (isset($_POST['donnees_utilisateur'])){
-                $donnees_utilisateur = $_POST['donnees_utilisateur'];
-                $requete = "INSERT INTO Utilisateurs VALUES ('','$nom_utilisateur','$prenom_utilisateur','$email_utilisateur','$mdp_utilisateur','$role_utilisateur','$organisme_utilisateur','$donnees_utilisateur')";
-                $result2 = mysqli_query($db, $requete) or die (mysqli_error($db)); //exécution de la requête
+        if($nom_utilisateur !== "" && $prenom_utilisateur !== "" && $email_utilisateur !== "" && $mdp_utilisateur !== "" && $role_utilisateur !== "" && $organisme_utilisateur !== ""){
+            $result = mysqli_query($db,"SELECT Email FROM Utilisateurs WHERE Email = '".$email_utilisateur."'");
+            $respond = mysqli_fetch_assoc($result);
+            if ($respond['Email'] == $email_utilisateur){
+                header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/inscription.php?erreur=2');
             }
             else{
-                unset($_POST['donnees_utilisateur']);
+                if (isset($_POST['donnees_utilisateur'])){
+                    $donnees_utilisateur = $_POST['donnees_utilisateur'];
+                    $requete = "INSERT INTO Utilisateurs VALUES ('','$nom_utilisateur','$prenom_utilisateur','$email_utilisateur','$mdp_utilisateur','$role_utilisateur','$organisme_utilisateur','$donnees_utilisateur')";
+                    $result2 = mysqli_query($db, $requete) or die (mysqli_error($db)); //exécution de la requête
+                }
+                else{
+                    $requete2 = "INSERT INTO Utilisateurs VALUES ('','$nom_utilisateur','$prenom_utilisateur','$email_utilisateur','$mdp_utilisateur','$role_utilisateur','$organisme_utilisateur','')";
+                    $result3 = mysqli_query($db, $requete2) or die (mysqli_error($db));
+                }
             }
-        }
-  ?>
+    ?>
     <h1>Devenez membre de MAIAT</h1>
     <p>&nbsp;</p>
     <h3>Vous etes bien inscrit ! </h3>
     <p> Maintenant vous pouvez vous  <a href="identification.php">Connecter</a></p>
-<?php
-    }
-    else{
-       header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/inscription.php?erreur=1'); // utilisateur ou mot de passe vide
-    }
-?>
-
-
+    <?php
+        }
+        else{
+           header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/inscription.php?erreur=1'); // utilisateur ou mot de passe vide
+        }
+    ?>
 </body>
 </html>
