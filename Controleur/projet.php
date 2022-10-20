e63e212c812279643be54f530567ef4f6e0e8336
charles <charles.cayzac@2022.icam.fr>
2022-10-20 15:03:07 +0200

    modif css projet

--- Controleur/projet.php
+++ Controleur/projet.php
@@ -23,32 +23,31 @@
                 </div>
             </header>
             <div class="projet">
-                <h1>MAIAT : L&#39;&eacute;quipe</h1>
-
-                <h3>C&#39;est dans le cadre du programme &quot;Confiance.ai&quot; que l&#39;outil MAIAT a &eacute;t&eacute; cr&eacute;&eacute;.</h3>
-
-                <img src="../Medias/logoconfiance.jpg">
-
-                <p>Pilier technologique du Grand D&eacute;fi &laquo; S&eacute;curiser, certifier et fiabiliser les syst&egrave;mes fond&eacute;s sur l&rsquo;intelligence artificielle &raquo; lanc&eacute; par l&rsquo;Etat,
-                    Confiance.ai est le plus gros programme de recherche technologique du plan #AIforHumanity qui doit faire de la France un des pays leader de
-                    l&rsquo;intelligence artificielle (IA).</p>
-
-                <p>&nbsp;</p>
-
-                <img src="../Medias/logo_spix.png" width="430" height="341">
-
-                <p>Spix industry est une start up fran&ccedil;aise qui a &eacute;t&eacute; cr&eacute;&eacute;e pour d&eacute;velopper l&rsquo;utilisation de la voix dans l&rsquo;industrie. Dans notre programme,
-                    SPIX tente de proposer &agrave; Renault un Assistant Vocal Intelligent &agrave; base d&#39;IA sur ses lignes de production.</p>
-
-                <img src="../Medias/logo_icam.png" width="430" height="341">
-
-                <p>C&#39;est dans le cadre d&#39;un m&eacute;moire scientifique de recherche que l&#39;&eacute;quipe Confiance s&#39;est pench&eacute;s sur la question de l&#39;acceptabilit&eacute; de l&#39;IA
-                    dans le monde du travail. Son r&ocirc;le est clairement de d&eacute;finir l&#39;acceptibilit&eacute; sociale d&#39;un tel ajout dans le professionnel.</p>
-
-                <img src="../Medias/logo_renault.png" width="430" height="341">
-
-                <p>Renault, leader mondial de la conception et la production automobile, participe &agrave; ce programme afin d&#39;introduire un assistant vocal intelligent
-                    sur ses lignes de productions.</p>
+                <div class="top">
+                    <h1>MAIAT : L&#39;&eacute;quipe</h1>
+                    <h3>C&#39;est dans le cadre du programme &quot;Confiance.ai&quot; que l&#39;outil MAIAT a &eacute;t&eacute; cr&eacute;&eacute;.</h3>
+                    <img src="../Medias/logoconfiance.jpg" width="150" height="106">
+                    <p>Pilier technologique du Grand D&eacute;fi &laquo; S&eacute;curiser, certifier et fiabiliser les syst&egrave;mes fond&eacute;s sur l&rsquo;intelligence artificielle &raquo; lanc&eacute; par l&rsquo;Etat,
+                        Confiance.ai est le plus gros programme de recherche technologique du plan #AIforHumanity qui doit faire de la France un des pays leader de
+                        l&rsquo;intelligence artificielle (IA).</p>
+                </div>
+                <div class="bottom">
+                    <div class="bottom-left">
+                        <img src="../Medias/logo_spix.png" >
+                        <p>Spix industry est une start up fran&ccedil;aise qui a &eacute;t&eacute; cr&eacute;&eacute;e pour d&eacute;velopper l&rsquo;utilisation de la voix dans l&rsquo;industrie. Dans notre programme,
+                            SPIX tente de proposer &agrave; Renault un Assistant Vocal Intelligent &agrave; base d&#39;IA sur ses lignes de production.</p>
+                    </div>
+                    <div class="bottom-center">
+                        <img src="../Medias/logo_icam.png" >
+                        <p>C&#39;est dans le cadre d&#39;un m&eacute;moire scientifique de recherche que l&#39;&eacute;quipe Confiance s&#39;est pench&eacute;s sur la question de l&#39;acceptabilit&eacute; de l&#39;IA
+                            dans le monde du travail. Son r&ocirc;le est clairement de d&eacute;finir l&#39;acceptibilit&eacute; sociale d&#39;un tel ajout dans le professionnel.</p>
+                    </div>
+                    <div class="bottom-right">
+                        <img src="../Medias/logo_renault.png" width="605" height="340">
+                        <p>Renault, leader mondial de la conception et la production automobile, participe &agrave; ce programme afin d&#39;introduire un assistant vocal intelligent
+                            sur ses lignes de productions.</p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
