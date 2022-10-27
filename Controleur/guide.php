17934a53e5826a0eae7f24e0ebcb58f5c7f2681a
charles <charles.cayzac@2022.icam.fr>
2022-10-27 10:54:34 +0200

    modif bandeau blog

--- Controleur/guide.php
+++ Controleur/guide.php
@@ -12,7 +12,7 @@
                 <div class="left">
                     <a href="https://www.confiance.ai/" class="logo"><img src="../Medias/logoconfiance.jpg" width="150" height="106"></a>
                 </div>
-                <div class="right">
+                <div class="middle">
                     <nav class="navbar">
                         <a href="https://dev2.icam.fr/toulouse/GEI/Confiance/index.php">MAIAT</a>
                         <a href="https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/critere.php">Les critères de risques sociaux</a>
@@ -20,6 +20,9 @@
                         <a href="https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/projet.php">Equipes et partenaires</a>
                         <a href="https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/identification.php" target="_blank">C'est à vous</a>
                     </nav>
+                </div>
+                <div class="right">
+                    <a href="https://www.icam.fr/" class="logo"><img src="../Medias/logo_icam_blanc.png" width="243" height="150"></a>
                 </div>
             </header>
             <div class="guide">
