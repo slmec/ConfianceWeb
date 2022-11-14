f32d2c3eae4e68908d41085d933f155e15e0c07d
charles <charles.cayzac@2022.icam.fr>
2022-11-14 16:21:05 +0100

    modif index + critere

--- index.php
+++ index.php
@@ -1,48 +1,47 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-    <link rel="stylesheet" href="Vue/style.css" />
-	<title>MAIAT</title>
-</head>
-<body>
-    <section>
-        <div class="container">
-            <header>
-                <div class="left">
-                    <a href="https://www.confiance.ai/" class="logo" target="_blank"><img src="Medias/logoconfiance.jpg" width="150" height="106"></a>
-                </div>
-                <div class="middle">
-                    <nav class="navbar">
-                        <a class="active" href="https://dev2.icam.fr/toulouse/GEI/Confiance/index.php">MAIAT</a>
-                        <a href="https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/critere.php">Les critères de risques sociaux</a>
-                        <a href="https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/guide.php">Le guide de MAIAT</a>
-                        <a href="https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/projet.php">Equipes et partenaires</a>
-                        <a href="https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/identification.php" target="_blank">C'est à vous</a>
-                    </nav>
-                </div>
-                <div class="right">
-                    <a href="https://www.icam.fr/" class="logo" target="_blank"><img src="Medias/logo_icam_blanc.png" width="243" height="150" ></a>
-                </div>
-            </header>
-            <div class="maiat">
-                <h1>MAIAT : Mesure de l'Acceptabilité Sociale de l&#39;Intelligence Artificielle au Travail</h1>
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+        <link rel="stylesheet" href="Vue/style.css" />
+        <title>MAIAT</title>
+    </head>
+    <body>
+        <section>
+            <div class="container">
+                <header>
+                    <div class="left">
+                        <a href="https://www.confiance.ai/" class="logo" target="_blank"><img src="Medias/logoconfiance.jpg" width="150" height="106"></a>
+                    </div>
+                    <div class="middle">
+                        <nav class="navbar">
+                            <a class="active" href="index.php">MAIAT</a>
+                            <a href="Controleur/critere.php">Les critères de risques sociaux</a>
+                            <a href="Controleur/guide.php">Le guide de MAIAT</a>
+                            <a href="Controleur/projet.php">Equipes et partenaires</a>
+                            <a href="Controleur/identification.php" target="_blank">C'est à vous</a>
+                        </nav>
+                    </div>
+                    <div class="right">
+                        <a href="https://www.icam.fr/" class="logo" target="_blank"><img src="Medias/logo_icam_blanc.png" width="243" height="150" ></a>
+                    </div>
+                </header>
+                <div class="maiat">
+                    <h1>MAIAT : Mesure de l'Acceptabilité Sociale de l&#39;Intelligence Artificielle au Travail</h1>
 
-                <video src="Medias/video_site.mp4" width=640px height=480px controls poster="Medias/vignette_video_site.jpg" class="video" alt="vidéo de présentation"></video>
+                    <video src="Medias/video_site.mp4" width=640px height=480px controls poster="Medias/vignette_video_site.jpg" class="video" alt="vidéo de présentation"></video>
 
-                <p>De nombreux travaux appr&eacute;hendent le d&eacute;ploiement de l&rsquo;IA dans les organisations et les m&eacute;tiers.&nbsp;Au-del&agrave; des dynamiques destructions/
-                    cr&eacute;ations d&rsquo;emplois, elles d&eacute;gagent des questions de transformation du travail et de leur acceptabilit&eacute;. Elles pointent en ce sens
-                    sur la n&eacute;cessit&eacute; d&rsquo;&eacute;tablir une confiance technique et sociale envers l&rsquo;IA. Cet outil propose un diagnostic social des applications
-                    de l&rsquo;IA au travail en vue de faciliter l&rsquo;instauration d&rsquo;une confiance situ&eacute;e.</p>
+                    <p>De nombreux travaux appr&eacute;hendent le d&eacute;ploiement de l&rsquo;IA dans les organisations et les m&eacute;tiers.&nbsp;Au-del&agrave; des dynamiques destructions/
+                        cr&eacute;ations d&rsquo;emplois, elles d&eacute;gagent des questions de transformation du travail et de leur acceptabilit&eacute;. Elles pointent en ce sens
+                        sur la n&eacute;cessit&eacute; d&rsquo;&eacute;tablir une confiance technique et sociale envers l&rsquo;IA. Cet outil propose un diagnostic social des applications
+                        de l&rsquo;IA au travail en vue de faciliter l&rsquo;instauration d&rsquo;une confiance situ&eacute;e.</p>
 
-                <img src="Medias/Prix.png" width=800px height=565px >
-                <p>Prix reçu le 1er Juillet 2022 à Saint-Étienne, France, lors de la Conférence Nationale sur les Applications Pratiques de l’Intelligence Artificielle</p>
+                    <img src="Medias/Prix.png" width=800px height=565px >
+                    <p>Prix reçu le 1er Juillet 2022 à Saint-Étienne, France, lors de la Conférence Nationale sur les Applications Pratiques de l’Intelligence Artificielle</p>
+                </div>
             </div>
-
-        </div>
-        <footer>
-            <p>Si vous souhaitez contacter notre &eacute;quipe, vous pouvez nous &eacute;crire &agrave; l&#39;adresse mail suivante : yann.ferguson@icam.fr</p>
-        </footer>
-    </section>
-</body>
+            <footer>
+                <p>Si vous souhaitez contacter notre &eacute;quipe, vous pouvez nous &eacute;crire &agrave; l&#39;adresse mail suivante : yann.ferguson@icam.fr</p>
+            </footer>
+        </section>
+    </body>
 </html>
