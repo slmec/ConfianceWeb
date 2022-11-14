4ba43a092aed047ec9f9c066d6bcbfd7811d5daa
Salomé <salome.catteau@2023.icam.fr>
2022-11-14 16:28:37 +0100

    hors ligne

--- Controleur/diagnostic_horsconnexion.php
+++ Controleur/diagnostic_horsconnexion.php
@@ -3,8 +3,7 @@
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
     <title>MAIAT</title>
-    <link rel="stylesheet" href="../Vue/style_fond_resultats_diagramme.css" />
-    <link rel="stylesheet" href="../Vue/style_resultats_analyse.css" />
+    <link rel="stylesheet" href="../Vue/style_cest_a_vous.css" />
 </head>
 <body onload="init();">
 <section>
@@ -14,22 +13,32 @@
                 <nav class="navbar">
                     <a href="../index.php" target="_blank" > MAIAT </a>
                     <a href="inscription.php">Inscription</a>
-                    <a class="active" href="identification.php">Connexion</a>
+                    <a  href="identification.php">Connexion</a>
                     <a href="testquestionnaire_sansid.php">Diagnostic sans connexion</a>
-                    <a href="diagnostic_horsconnexion.php">Diagnostic hors ligne</a>
+                    <a class="active" href="diagnostic_horsconnexion.php">Diagnostic hors ligne</a>
                 </nav>
             </header>
         </div>
     </div>
-    <div class="block_texte">
-        <p>Vous avez choisit de ne pas r&eacute;aliser le test via notre outil en ligne.</p>
+    <div class ="block_page">
+        <div class ="block_titre">
+            <h1>Diagnostic hors ligne</h1>
+            <br>
+        </div>
 
-        <p>Pour ce faire vous devez faire la demande par mail sur le mail suivant : yann.ferguson@icam.fr</p>
-
-        <p>Par la suite, vous recevrez dans les meilleurs d&eacute;lais une macro excel.</p>
-
-        <p>Celle-ci est une version hors-ligne de l&#39;outil de mesure d&#39;acceptabilit&eacute; sociale de l&#39;IA au travail.&nbsp;</p>
-    </div>
+        <div class ="block_form">
+            <form action="" >
+                <p>Vous avez choisit de ne pas r&eacute;aliser le test via notre outil en ligne.</p>
+                <p>Pour ce faire vous devez faire la demande par mail sur le mail suivant : <strong>yann.ferguson@icam.fr</strong></p>
+                <p>Par la suite, vous recevrez dans les meilleurs d&eacute;lais <strong>un fichier excel</strong>.</p>
+                <p>Celle-ci est une version hors-ligne de l&#39;outil de mesure d&#39;acceptabilit&eacute; sociale de l&#39;IA au travail.&nbsp;</p>
+            </form>
+        </div>
+        <div class="bas">
+            <br>
+            <p class="bas">Si vous souhaitez contacter notre &eacute;quipe, vous pouvez nous &eacute;crire &agrave; l&#39;adresse mail suivante : yann.ferguson@icam.fr</p>
+            <br>
+        </div>
 </section>
 </body>
 
