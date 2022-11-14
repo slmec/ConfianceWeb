459373c884676909c642b899c54f3b10ee8c350a
charles <charles.cayzac@2022.icam.fr>
2022-11-14 15:28:54 +0100

    modif liens

---
+++ Controleur/diagnostic_horsconnexion.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title>MAIAT</title>
+    <link rel="stylesheet" href="../Vue/style_fond_resultats_diagramme.css" />
+    <link rel="stylesheet" href="../Vue/style_resultats_analyse.css" />
+</head>
+<body onload="init();">
+<section>
+    <div class="block_entete">
+        <div class="container">
+            <header>
+                <nav class="navbar">
+                    <a href="../index.php" target="_blank" > MAIAT </a>
+                    <a href="inscription.php">Inscription</a>
+                    <a class="active" href="identification.php">Connexion</a>
+                    <a href="testquestionnaire_sansid.php">Diagnostic sans connexion</a>
+                    <a href="diagnostic_horsconnexion.php">Diagnostic hors ligne</a>
+                </nav>
+            </header>
+        </div>
+    </div>
+    <div class="block_texte">
+        <p>Vous avez choisit de ne pas r&eacute;aliser le test via notre outil en ligne.</p>
+
+        <p>Pour ce faire vous devez faire la demande par mail sur le mail suivant : yann.ferguson@icam.fr</p>
+
+        <p>Par la suite, vous recevrez dans les meilleurs d&eacute;lais une macro excel.</p>
+
+        <p>Celle-ci est une version hors-ligne de l&#39;outil de mesure d&#39;acceptabilit&eacute; sociale de l&#39;IA au travail.&nbsp;</p>
+    </div>
+</section>
+</body>
+
