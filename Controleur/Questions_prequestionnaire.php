e5c1fcc38f74392bc724098fd636e09a9f20d54e
Salomé <salome.catteau@2023.icam.fr>
2022-11-28 09:48:40 +0100

    modif Q3 pre-questionnaire

--- Controleur/Questions_prequestionnaire.php
+++ Controleur/Questions_prequestionnaire.php
@@ -43,7 +43,7 @@
                     <br>
                     <p>Dans quel contexte est utilis&eacute; le syst&egrave;me &agrave; base d&#39;IA ?&nbsp;<input maxlength="500" name="Contexte_casusage" type="text" /></p>
                     <p>Quel est l&#39;objectif du syst&egrave;me &agrave; base d&#39;IA ?&nbsp;<input maxlength="500" name="Objectif_sia" type="text" /></p>
-                    <p>Comment ce syst&egrave;me &agrave; base d&#39;IA fonctionne-t-il ?&nbsp;<input maxlength="500" name="Fonctionnement_sia" type="text" /></p>
+                    <p>Quelle est l'interaction entre l’homme et le syst&egrave;me &agrave; base d&#39;IA ?&nbsp;<input maxlength="500" name="Fonctionnement_sia" type="text" /></p>
                     <p>Par qui est-il utilis&eacute; ?&nbsp;<input maxlength="500" name="Utilisation_sia" type="text" /></p>
                     <p>Quelle est la maturit&eacute; actuelle de votre syst&egrave;me &agrave; base d&#39;IA ? :</p>
                     <br>
