b4557d9380e5feda6b1d143a28da58b95c4b4619
charles <charles.cayzac@2022.icam.fr>
2022-12-05 15:12:07 +0100

    changement prequestionnaire

--- Modele/verification_question_prequestionnaire.php
+++ Modele/verification_question_prequestionnaire.php
@@ -10,14 +10,14 @@
     or die('could not connect to database');
 
     //Questions_prequestionnaire.php
-    if ($_POST['Contexte_casusage'] !== "" && $_POST['Objectif_sia'] !== "" && $_POST['Fonctionnement_sia'] !== "" && $_POST['Fonctionnement_sia'] !== "" && $_POST['Fonctionnement_sia'] !== "") {
+    if ($_POST['Contexte_casusage'] !== "" && $_POST['Objectif_sia'] !== "" && $_POST['Fonctionnement_sia'] !== "" && $_POST['Utilisation_sia'] !== "" && $_POST['Maturite'] !== "") {
 
 
         $_SESSION['Contexte_casusage'] = $_POST['Contexte_casusage'];
         $_SESSION['Objectif_sia'] = $_POST['Objectif_sia'];
         $_SESSION['Fonctionnement_sia'] = $_POST['Fonctionnement_sia'];
-        $_SESSION['Utilisation_sia'] = $_POST['Fonctionnement_sia'];
-        $_SESSION['Maturite'] = $_POST['Fonctionnement_sia'];
+        $_SESSION['Utilisation_sia'] = $_POST['Utilisation_sia'];
+        $_SESSION['Maturite'] = $_POST['Maturite'];
 
 
                 header('Location: https://dev2.icam.fr/toulouse/GEI/Confiance/Controleur/testquestionnaire.php');
