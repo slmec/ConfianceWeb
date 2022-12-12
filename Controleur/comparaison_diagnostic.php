c366efcc199fb640e65eb2b19674499ae4e745f5
Salomé <salome.catteau@2023.icam.fr>
2022-12-12 15:49:32 +0100

    connexion bdd

--- Controleur/comparaison_diagnostic.php
+++ Controleur/comparaison_diagnostic.php
@@ -1,14 +1,5 @@
 <!DOCTYPE html>
-<?php
-    session_start();
-    $db_username = 'eleve.tou';
-    $db_password = 'et*301';
-    $db_name     = 'Confiance';
-    $db_host     = 'localhost';
-
-    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
-    or die('could not connect to database');
-?>
+<?php include("connexion.php"); ?>
 <html>
     <head>
         <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
